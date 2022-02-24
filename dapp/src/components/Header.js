3775d194a21b90045bcc3408d7102553f61cdedb
German Navarro <g.manavarro@gmail.com>
2022-02-24 15:44:02 -0300

    Fix header

--- dapp/src/components/Header.js
+++ dapp/src/components/Header.js
@@ -8,16 +8,16 @@ const Header = () => {
       style={{
         display: 'flex',
         alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
       }}
     >
       <img
         alt="Extrimian logo"
         src="https://www.extrimian.com/assets/img/extrimian_logo.svg"
-        style={{ maxWidth: '15em' }}
+        style={{ maxWidth: '15em', position: 'absolute', left: '2em', top: 0 }}
       />
-      <Title style={{ color: 'white', margin: 0, paddingLeft: '3em' }}>
-        Extrimian Challenge
-      </Title>
+      <Title style={{ color: 'white', margin: 0 }}>Extrimian Challenge</Title>
     </AntdHeader>
   );
 };
