532b32074efac22ba0aedfc5d4914b28c10f36db
German Navarro <g.manavarro@gmail.com>
2022-02-28 11:57:30 -0300

    Remove unnecesary code

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,16 +1,11 @@
-import { getMessage, setMessage } from './services/api';
-import { useState } from 'react';
-
 import Header from './components/Header';
 import Content from './components/Content';
 
 function App() {
-  const [messageText, setMessageText] = useState('');
-  const [inputValue, setInputValue] = useState(messageText);
 
   return (
     <div
-      className="App"
+      className='App'
       style={{
         display: 'flex',
         flexDirection: 'column',
